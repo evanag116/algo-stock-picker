feb8b2aa2c362a1a7a1d39d580613aca475fad1d
Evan Garcia <evanag116@yahoo.com>
2022-06-06 15:04:20 -0500

    completed in py

--- python/stock_picker.py
+++ python/stock_picker.py
@@ -1,2 +1,18 @@
 def picker(prices):
-    pass
+    l, r = 0, 1
+    max_profit = 0
+    answer = []
+
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            if profit > max_profit:
+                max_profit = profit
+                answer.append([l, r])
+        else:
+            l = r
+        r += 1
+
+    return answer[-1]
+
+
